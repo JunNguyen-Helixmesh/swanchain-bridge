6ec2e57a77d016eb88a5bb77ca67d962f7d2ccdf
James Chen <jameschen@jamess-mbp.home>
2024-01-15 15:19:24 -0500

    refactored store and some utility files

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import counterSlice from "./redux/reducer/reducer"
 import walletSlice from './redux/reducer/disconnectReducer'
+
 export const store = configureStore({
   reducer: {
     address: counterSlice,
     wallet: walletSlice
   },
 })
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch

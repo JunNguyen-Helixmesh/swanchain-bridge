4e0f4a8baf8187e444f0607d7b0f40e5e5496ea5
James Chen <jameschen@jamess-mbp.home>
2024-01-17 17:46:24 -0500

    remanaged dependencies

--- next.config.js
+++ next.config.js
@@ -5,6 +5,17 @@ const nextConfig = {
   env: {
     // env variables
   },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;

b8e9484e161e3728c3fe5fee493c175d5f99bf41
rykci <50771299+rykci@users.noreply.github.com>
2024-05-05 20:55:54 -0400

    handle prove withdraw

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 ## Orchestrator bridge frontend
 
-TODO: ~~Rewrite & restyling of code from 
+TODO: ~~Rewrite & restyling of code from
 https://github.com/nitantchhajed/op-stack-bridge/tree/master~~
+
 - ~~Figure out why page routing is not working.~~
-- ~~Make adjustments to _app as wagmiconfig is deprecated~~
+- ~~Make adjustments to \_app as wagmiconfig is deprecated~~
 - ~~Fix stretched assets~~
-- Fix wallet login state persisting across routes 
+- Fix wallet login state persisting across routes
 - Fix switch to swan/sepolia button
 - Fix accounts routing
 - Fix metamask button and icon
@@ -16,20 +17,23 @@ https://github.com/nitantchhajed/op-stack-bridge/tree/master~~
 - Fix depositing to Racenet instead of Swan
 - Fix NaN ETH display on withdraw route
 
+- get wallet address on landing withdraw-history
+
 ## Notes
 
 - Create a .env.production file in root directory with the appropriate fields filled in.
 
 - Rewritten for use of wagmi@2.2.1 and ethers 6.10.1 with TypeScript and NextJS
- - [useSwitchNetwork](0.5.x.wagmi.sh/react/hooks/useSwitchNetwork) is now [useSwitchChain](wagmi.sh/react/api/hooks/useSwitchChain)
- - Types require using TypeScript >=5.0.4.
- - Since TypeScript does not follow semantic versioning it is highly recommended to lock Wagmi and TypeScript versions to specific patch releases and consider the possibility of types being fixed or upgraded between releases
- - tsconfig.json should set strict to true in compilerOptions
- - [ethers v6 uses ethers.JsonRPCProvider instead of ethers.providers.JsonRPCProvider](docs.ethers.org/v6/migrating/)
- - [ethers v6 uses BrowserProvider instead of Web3Provider](docs.ethers.org/migrating/#migrate-providers)
+- [useSwitchNetwork](0.5.x.wagmi.sh/react/hooks/useSwitchNetwork) is now [useSwitchChain](wagmi.sh/react/api/hooks/useSwitchChain)
+- Types require using TypeScript >=5.0.4.
+- Since TypeScript does not follow semantic versioning it is highly recommended to lock Wagmi and TypeScript versions to specific patch releases and consider the possibility of types being fixed or upgraded between releases
+- tsconfig.json should set strict to true in compilerOptions
+- [ethers v6 uses ethers.JsonRPCProvider instead of ethers.providers.JsonRPCProvider](docs.ethers.org/v6/migrating/)
+- [ethers v6 uses BrowserProvider instead of Web3Provider](docs.ethers.org/migrating/#migrate-providers)
 - [useNetwork](0.5.x.wagmi.sh/react/hooks/useNetwork) depracated for [useAccount](wagmi.sh/react/guides/migrate-from-v1-to-v2#removed-usenetwork-hook)
 
 Run the following commands to build
+
 ```
 docker build -t orchestrator-bridge-ui .
 docker run -p 3000:3000 orchestrator-bridge-ui
@@ -38,7 +42,9 @@ docker run -p 3000:3000 orchestrator-bridge-ui
 or
 
 ```
+
 docker pull jameschennbai/orchestratorbridge-ui:platformfix
 
 ```
 https://hub.docker.com/layers/jameschennbai/orchestratorbridge-ui/platformfix/images/sha256-68891b8e6861e27402442169a2d638cbafd83376c42749cb62ac8867afe7cbff?context=repo
+```
--- src/components/WithdrawHistory.tsx
+++ src/components/WithdrawHistory.tsx
@@ -2,12 +2,15 @@ import React, { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 
 import Head from 'next/head'
+import { Spinner } from 'react-bootstrap'
 import { GrSend } from 'react-icons/gr'
 import { IoMdTime } from 'react-icons/io'
 import { LuShield } from 'react-icons/lu'
 import { HiDownload, HiDotsVertical } from 'react-icons/hi'
+import { useAccount } from 'wagmi'
 
 const { ethers } = require('ethers')
+const optimismSDK = require('@eth-optimism/sdk')
 // const Web3 = require('web3')
 
 const OUTPUT_ORACLE_ABI = [
@@ -27,7 +30,9 @@ const OUTPUT_ORACLE_ABI = [
 ]
 
 const WithdrawHistory: React.FC = (walletAddress: any) => {
+  const { address, isConnected } = useAccount()
   const [withdrawals, setWithdrawals] = useState([])
+  const [loader, setLoader] = useState<boolean>(false)
   const [modalData, setModalData] = useState<any>(null)
   const router = useRouter()
 
@@ -53,6 +58,12 @@ const WithdrawHistory: React.FC = (walletAddress: any) => {
 
   const closeModal = () => {
     setModalData(null)
+  }
+
+  const checkModalClick = (e: any) => {
+    if (e.target.className == 'modal-container') {
+      closeModal()
+    }
   }
 
   const getModalData = async (rowData: any) => {
@@ -85,6 +96,15 @@ const WithdrawHistory: React.FC = (walletAddress: any) => {
       )
       console.log(rowData)
 
+      if (
+        rowData.latestOutputtedBlockNumber < Number(rowData.block_number) ||
+        rowData.status == 'finalized'
+      ) {
+        rowData.isButtonDisabled = true
+      } else {
+        rowData.isButtonDisabled = false
+      }
+
       // Get withdraw transaction details
       const transaction = await l2Provider.getTransaction(rowData.tx_hash)
 
@@ -100,6 +120,82 @@ const WithdrawHistory: React.FC = (walletAddress: any) => {
     }
 
     setModalData(rowData)
+  }
+
+  const handleModalButton = async () => {
+    let l1Url = process.env.NEXT_PUBLIC_L1_RPC_URL
+    let l2Url = process.env.NEXT_PUBLIC_L2_RPC_URL
+    let AddressManager = process.env.NEXT_PUBLIC_LIB_ADDRESSMANAGER
+    let L1CrossDomainMessenger =
+      process.env.NEXT_PUBLIC_PROXY_OVM_L1CROSSDOMAINMESSENGER
+    let L1StandardBridge = process.env.NEXT_PUBLIC_PROXY_OVM_L1STANDARDBRIDGE
+    let L2OutputOracle = process.env.NEXT_PUBLIC_L2_OUTPUTORACLE_PROXY
+    let OptimismPortal = process.env.NEXT_PUBLIC_OPTIMISM_PORTAL_PROXY
+    if (modalData.chain_id == '20241133') {
+      l2Url = process.env.NEXT_PUBLIC_L2_PROXIMA_RPC_URL
+      AddressManager = process.env.NEXT_PUBLIC_PROXIMA_LIB_ADDRESSMANAGER
+      L1CrossDomainMessenger =
+        process.env.NEXT_PUBLIC_PROXIMA_PROXY_OVM_L1CROSSDOMAINMESSENGER
+      L1StandardBridge =
+        process.env.NEXT_PUBLIC_PROXIMA_PROXY_OVM_L1STANDARDBRIDGE
+      L2OutputOracle = process.env.NEXT_PUBLIC_L2_PROXIMA_OUTPUTORACLE_PROXY
+      OptimismPortal = process.env.NEXT_PUBLIC_PROXIMA_OPTIMISM_PORTAL_PROXY
+    }
+    const l1Provider = new ethers.providers.Web3Provider(window.ethereum)
+    const l2Provider = new ethers.providers.JsonRpcProvider(l2Url, 'any')
+    const l1Signer = l1Provider.getSigner(address)
+    const l2Signer = l2Provider.getSigner(address)
+    const zeroAddr = '0x'.padEnd(42, '0')
+    const l1Contracts = {
+      StateCommitmentChain: zeroAddr,
+      CanonicalTransactionChain: zeroAddr,
+      BondManager: zeroAddr,
+      AddressManager,
+      L1CrossDomainMessenger,
+      L1StandardBridge,
+      OptimismPortal,
+      L2OutputOracle,
+    }
+    const crossChainMessenger = new optimismSDK.CrossChainMessenger({
+      contracts: {
+        l1: l1Contracts,
+      },
+      // bridges: bridges,
+      l1ChainId: Number(process.env.NEXT_PUBLIC_L1_CHAIN_ID),
+      l2ChainId: Number(process.env.NEXT_PUBLIC_L2_CHAIN_ID),
+      l1SignerOrProvider: l1Signer,
+      l2SignerOrProvider: l2Signer,
+      // bedrock: true,
+    })
+    console.log(address)
+
+    try {
+      setLoader(true)
+      const response = await crossChainMessenger.proveMessage(modalData.tx_hash)
+      await response.wait()
+
+      console.log('withdraw response:', response)
+
+      const crossChainMessage = await crossChainMessenger.toCrossChainMessage(
+        response,
+      )
+
+      console.log('crosschain message:', crossChainMessage)
+      const transactionHash = crossChainMessage.transactionHash
+
+      if (transactionHash !== null) {
+        setLoader(false)
+
+        // if (isConnected && address) {
+        // await updateWithdrawHistory(address, transactionHash, blockNumber)
+        // await callGalxeAPI();
+        // }
+        // setTimeout(fetchBalance, 3000)
+      }
+    } catch (error) {
+      console.error('Error:', error.message)
+      return 'Error occurred while fetching transaction value'
+    }
   }
 
   return (
@@ -139,7 +235,7 @@ const WithdrawHistory: React.FC = (walletAddress: any) => {
         </table>
       </div>
       {modalData && (
-        <div className="modal-container" onClick={() => setModalData(null)}>
+        <div className="modal-container" onClick={checkModalClick}>
           <div className="modal">
             <div className="modal-content">
               <div className="modal-content-header">
@@ -161,7 +257,13 @@ const WithdrawHistory: React.FC = (walletAddress: any) => {
                   <li className="vertical-dots">
                     <HiDotsVertical />
                   </li>
-                  <li className="withdraw-step done">
+                  <li
+                    className={
+                      modalData.isButtonDisabled
+                        ? 'withdraw-step'
+                        : 'withdraw-step done'
+                    }
+                  >
                     <IoMdTime size={28} />
                     Wait for the published withdraw on L1
                   </li>
@@ -170,8 +272,7 @@ const WithdrawHistory: React.FC = (walletAddress: any) => {
                   </li>
                   <li
                     className={
-                      Number(modalData.latestOutputtedBlockNumber) >=
-                      Number(modalData.block_number)
+                      modalData.status == 'proved'
                         ? 'withdraw-step done'
                         : 'withdraw-step'
                     }
@@ -197,7 +298,7 @@ const WithdrawHistory: React.FC = (walletAddress: any) => {
                   </li>
                   <li
                     className={
-                      modalData.status == 'proved'
+                      modalData.status == 'finalized'
                         ? 'withdraw-step done'
                         : 'withdraw-step'
                     }
@@ -209,15 +310,23 @@ const WithdrawHistory: React.FC = (walletAddress: any) => {
               </div>
               <div className="modal-btn-container">
                 <button
-                  className="modal-btn"
-                  disabled={
-                    Number(modalData.latestOutputtedBlockNumber) <
-                    Number(modalData.block_number)
+                  className={
+                    modalData.isButtonDisabled
+                      ? 'modal-btn disabled'
+                      : 'modal-btn'
                   }
+                  disabled={modalData.isButtonDisabled}
+                  onClick={() => handleModalButton()}
                 >
-                  {modalData.status == 'initiated'
-                    ? 'Prove withdrawal'
-                    : 'Claim withdrawal'}
+                  {loader ? (
+                    <Spinner animation="border" role="status">
+                      <span className="visually-hidden">Loading...</span>
+                    </Spinner>
+                  ) : modalData.status == 'initiated' ? (
+                    'Prove withdrawal'
+                  ) : (
+                    'Claim withdrawal'
+                  )}
                 </button>
               </div>
             </div>
